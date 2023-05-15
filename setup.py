150846af118de81a6d0fa34c9b0e362557be2dd4
Youssef Khemiri <youssefkhemiri2001@gmail.com>
2023-05-15 01:20:12 +0100

    v2

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-# from setuptools import setup
+from setuptools import setup
 
-# # from streamlit_google_oauth import __version__
+# from streamlit_google_oauth import __version__
 
-# setup(
-#     name="streamlit_google_oauth",
-#     version="0.1",
-#     url="https://github.com/hunkim/streamlit-google-oauth",
-#     author="Sung Kim",
-#     author_email="hunkim@gmail.com",
-#     py_modules=["streamlit_google_oauth"],
-#     packages=["streamlit_google_oauth"],
-#     install_requires=[
-#         "protobuf<=3.20.0",
-#         "streamlit",
-#         "httpx-oauth",
-#         "typing-extensions",
-#     ],
-# )
+setup(
+    name="streamlit_google_oauth",
+    version="0.1",
+    url="https://github.com/hunkim/streamlit-google-oauth",
+    author="Sung Kim",
+    author_email="hunkim@gmail.com",
+    py_modules=["streamlit_google_oauth"],
+    packages=["streamlit_google_oauth"],
+    install_requires=[
+        "protobuf<=3.20.0",
+        "streamlit",
+        "httpx-oauth",
+        "typing-extensions",
+    ],
+)
